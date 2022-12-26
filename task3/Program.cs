8f56224c12f458bdbdb9ffe035c9bd15f87be5f3
Vika <vika.tulnikova@bk.ru>
2022-12-26 19:23:37 +0300

    add task3

---
+++ task3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число от 1 до 7 для вывода выходного дня недели");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1 || number > 7)
+{
+    System.Console.WriteLine($"{number} это число не является днём недели");
+}
+
+else if (number <= 5)
+{
+    System.Console.WriteLine($"{number} это число является будним днём недели");
+}
+
+else
+{
+    System.Console.WriteLine($"{number} это число является выходным днём недели");
+}
