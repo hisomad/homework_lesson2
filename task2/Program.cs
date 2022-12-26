a844e846e4ddfdf07d36ff5cf3f070a9ed5a5ae3
Vika <vika.tulnikova@bk.ru>
2022-12-26 19:20:03 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите трёхзначное число");
+
+int number = 456;
+
+if (number / 100 == 0)
+{
+    System.Console.WriteLine("Третьей цифры нет");
+    return;
+}
+
+    while (number > 999)
+    {
+        number /= 10; 
+    }
+
+System.Console.WriteLine(number % 10);
