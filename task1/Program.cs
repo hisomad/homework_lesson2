08470ec000cc616d03d1c3f1e145ab6ed3951bf4
Vika <vika.tulnikova@bk.ru>
2022-12-26 19:12:54 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.WriteLine("Введите трёхзначное число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int firstNumber = number / 10;
+int secondNumber = firstNumber % 10;
+
+System.Console.WriteLine(secondNumber);
